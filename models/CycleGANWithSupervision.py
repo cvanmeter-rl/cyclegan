007eb00b6d5996a03b392ac2b10186d26d2e44c0
Christian VanMeter <72093440+cvanmeter-rl@users.noreply.github.com>
2025-11-16 17:51:49 -0600

    Update CycleGANWithSupervision.py

--- models/CycleGANWithSupervision.py
+++ models/CycleGANWithSupervision.py
@@ -193,7 +193,8 @@ class CycleGANWithSupervision(BaseModel):
             target = labelmap(target, oem_label_rules)
             target = labelmap(target,oem_to_dfc19_rules) 
             target = torch.from_numpy(target.astype(np.int64)).to(self.device)
-            
+            print(f'target shape {target.shape}')
+            print(f'logits shape {logits.shape}')
             if logits.shape[-2:] != target.shape[-2:]:
                 print('logits shape doesnt match')
                 target = F.interpolate(
